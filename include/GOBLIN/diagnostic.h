47368fca932a3c9ac2546b1fee24a567e566f3d3
Kacper Tucholski <kacper.tucholski.kt@gmail.com>
2026-04-18 02:06:39 +0200

    Added diagnostics macros

---
+++ include/GOBLIN/diagnostic.h
@@ -0,0 +1,43 @@
+#ifndef DIAGNOSTIC_H
+#define DIAGNOSTIC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DIAG_PRINT(prefix, ...)              \
+do {                                         \
+    fprintf(stderr, prefix " %s:%d (%s): ",  \
+            __FILE__, __LINE__, __func__);   \
+    fprintf(stderr, __VA_ARGS__);            \
+    fprintf(stderr, "\n"); \
+} while (0)
+
+/* Public macros */
+
+#define UNIMPLEMENTED(...)                      \
+do {                                            \
+    DIAG_PRINT("UNIMPLEMENTED:", __VA_ARGS__);  \
+    abort();                                    \
+} while (0)
+
+#define TODO(...)                      \
+do {                                   \
+    DIAG_PRINT("TODO:", __VA_ARGS__);  \
+    abort();                           \
+} while (0)
+
+#define UNREACHABLE(...)                      \
+do {                                          \
+    DIAG_PRINT("UNREACHABLE:", __VA_ARGS__);  \
+    abort();                                  \
+} while (0)
+
+/* Optional: for switch fallthrough safety */
+#define ASSERT_NOT_REACHED()                             \
+do {                                                     \
+    fprintf(stderr, "ASSERT_NOT_REACHED: %s:%d (%s)\n",  \
+            __FILE__, __LINE__, __func__);               \
+    abort();                                             \
+} while (0)
+
+#endif // DIAGNOSTIC_H
